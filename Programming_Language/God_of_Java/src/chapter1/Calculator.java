9ad8748b4f7cec570dba5dde7406f4e693db92a7
Noctua99 <jonginchoi99@gmail.com>
2021-10-03 22:05:05 +0900

    2021_10_03

---
+++ Programming_Language/God_of_Java/src/chapter1/Calculator.java
@@ -0,0 +1,15 @@
+package chapter1;
+
+public class Calculator {
+    public int add(int num1, int num2) {
+        int sum;
+        sum = num1 + num2;
+        return sum;
+    }
+
+    public int addAll(int num1, int num2, int num3) {
+        int sum;
+        sum = num1 + num2 + num3;
+        return sum;
+    }
+}
