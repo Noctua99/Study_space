b0577dc376e48591c5bab403ed377195e295d197
Noctua99 <jonginchoi99@gmail.com>
2021-10-17 20:14:14 +0900

    2021_10_17

--- Algorithm/AlgorithmWithMosh/src/WorkingWithLinkedList.java
+++ Algorithm/AlgorithmWithMosh/src/WorkingWithLinkedList.java
@@ -1,5 +1,4 @@
 import java.util.Arrays;
-import java.util.LinkedList;
 
 public class WorkingWithLinkedList {
     public static void main(String[] args) {
@@ -9,6 +8,13 @@ public class WorkingWithLinkedList {
         list.addLast(30);
         System.out.println(list.contains(10));
         System.out.println(list.indexOf(10));
+        list.print();
+        list.removeFirst();
+        list.print();
+        list.addFirst(10);
+        list.print();
+        list.removeLast();
+        list.print();
         System.out.println(list.size());
         var array = list.toArray();
         System.out.println(Arrays.toString(array));
