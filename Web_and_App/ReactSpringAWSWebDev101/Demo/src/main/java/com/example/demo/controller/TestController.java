8775125d7b422dbd9cf82b08e8fe4924d07e43e3
Noctua99 <jonginchoi99@gmail.com>
2022-01-17 20:24:44 +0900

    2022_01_17

--- Web_and_App/ReactSpringAWSWebDev101/Demo/src/main/java/com/example/demo/controller/TestController.java
+++ Web_and_App/ReactSpringAWSWebDev101/Demo/src/main/java/com/example/demo/controller/TestController.java
@@ -1,18 +1,34 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.demo.dto.TestRequestBodyDTO;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("test") // 리소스
 public class TestController {
+
     @GetMapping
-    public String testController(){
+    public String testController() {
         return "Hello World";
     }
+
     @GetMapping("/testGetMapping")
-    public String testControllerWithPath(){
+    public String testControllerWithPath() {
         return "Hello World! testGetMapping";
+    }
+
+    @GetMapping("/{id}")
+    public String testControllerWithPathVariables(@PathVariable(required = false) int id) {
+        return "Hello World! ID " + id;
+    }
+
+    @GetMapping("/testRequestParam")
+    public String testControllerRequestParam(@RequestParam(required = false) int id) {
+        return "Hello World! ID " + id;
+    }
+
+    @GetMapping("/testRequestBody")
+    public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
+        return "Hello World! ID " + testRequestBodyDTO.getId() + " Message : " + testRequestBodyDTO.getMessage();
     }
 }
