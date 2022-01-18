34922ad83bb4fa4630b8a2bce4a75b30250a46ac
Noctua99 <jonginchoi99@gmail.com>
2022-01-18 20:25:51 +0900

    2022_01_18

--- Web_and_App/ReactSpringAWSWebDev101/Demo/src/main/java/com/example/demo/controller/TestController.java
+++ Web_and_App/ReactSpringAWSWebDev101/Demo/src/main/java/com/example/demo/controller/TestController.java
@@ -1,7 +1,12 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.TestRequestBodyDTO;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("test") // 리소스
@@ -30,5 +35,22 @@ public class TestController {
     @GetMapping("/testRequestBody")
     public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {
         return "Hello World! ID " + testRequestBodyDTO.getId() + " Message : " + testRequestBodyDTO.getMessage();
+    }
+
+    @GetMapping("/testResponseBody")
+    public ResponseDTO<String> testControllerResponseBody() {
+        List<String> list = new ArrayList<>();
+        list.add("Hello World! I'm ResponseDTO");
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return response;
+    }
+
+    @GetMapping("/testResponseEntity")
+    public ResponseEntity<?> testControllerResponseEntity() {
+        List<String> list = new ArrayList<>();
+        list.add("Hello World! I'm ResponseEntity. And you got 400!");
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        // http status를 400으로 설정
+        return ResponseEntity.badRequest().body(response);
     }
 }
