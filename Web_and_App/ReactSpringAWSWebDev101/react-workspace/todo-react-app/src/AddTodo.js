d051475440ce761f6ece9f42d0a9c049a1b4dc82
Noctua99 <jonginchoi99@gmail.com>
2022-01-31 20:22:38 +0900

    2022_01_31

--- Web_and_App/ReactSpringAWSWebDev101/react-workspace/todo-react-app/src/AddTodo.js
+++ Web_and_App/ReactSpringAWSWebDev101/react-workspace/todo-react-app/src/AddTodo.js
@@ -5,6 +5,25 @@ class AddTodo extends React.Component {
   constructor(props) {
     super(props);
     this.state = { item: { title: "" } }; // 사용자의 입력을 저장할 오브젝트
+    this.add = props.add;
+  }
+
+  onInputChange = (e) => {
+    const thisItem = this.state.item;
+    thisItem.title = e.target.value;
+    this.setState({ item: thisItem });
+    console.log(thisItem);
+  }
+
+  onButtonClick = () => {
+    this.add(this.state.item);
+    this.setState({ item: { title: "" } });
+  }
+
+  enterKeyEventHandler = (e) => {
+    if (e.key === 'Enter') {
+      this.onButtonClick();
+    }
   }
 
   render() {
@@ -12,10 +31,19 @@ class AddTodo extends React.Component {
       <Paper style={{ margin: 16, padding: 16 }}>
         <Grid container>
           <Grid xs={11} md={11} item style={{ paddingRight: 16 }}>
-            <TextField placeholder="Add Todo here" fullWidth />
+            <TextField
+              placeholder="Add Todo here"
+              fullWidth
+              onChange={this.onInputChange}
+              value={this.state.item.title}
+              onKeyPress={this.enterKeyEventHandler} />
           </Grid>
           <Grid xs={1} md={1} item>
-            <Button fullWidth color="secondary" variant="outlined">
+            <Button
+              fullWidth color="secondary"
+              color="secondary"
+              variant="outlined"
+              onClick={this.onButtonClick}>
               +
             </Button>
           </Grid>
